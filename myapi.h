214ed1e84af2a94dc37e310f6993046a43de3dd9
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-06-15 04:56:15 -0400

    First do this

--- myapi.h
+++ myapi.h
@@ -2,12 +2,10 @@
 #define _MY_MAIN_H
 struct MyOpts;
 int my_main(struct MyOpts *opts);
-enum Command { SECOND_CMD, FIRST_CMD, };
+enum Command { SUM, PRODUCT};
 struct MyOpts {
    enum Command cmd;
    char *a_path;
-   int a_flag;
-   float float_param;
    int nb_elements;
    char **elements;
 };
